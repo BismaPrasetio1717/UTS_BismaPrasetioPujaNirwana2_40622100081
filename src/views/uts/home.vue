e91ba328460536ccc8ca5a5ba353a88a0484478a
BismaPrasetio1717 <161411135+BismaPrasetio1717@users.noreply.github.com>
2024-05-06 22:48:47 +0700

    first commit

---
+++ src/views/uts/home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>SELAMAT DATANG DI APLIKASI</h1>
+</br>
+<h2>Aplikasi ini dibuat oleh : </h2>
+</br>
+<h4>Nama : Bisma Prasetio Puja Nirwana</h4>
+</br>
+<h4>NPM : 4062210081</h4><br>
+</template>
